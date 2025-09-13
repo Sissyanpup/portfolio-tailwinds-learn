bfe222007e54b37831f8c553b0d3ef4e7e5c02bd
Sissyanpup <muhammadzirldaprairi99@gmail.com>
2025-09-14 00:17:29 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# portfolio-tailwinds-learning
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#f43f5e',
+        dark: '#be185d',
+        secondery: '#64748b',
+      }
+    },
+  },
+  plugins: [],
+}
+
